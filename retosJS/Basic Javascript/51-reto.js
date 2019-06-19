28ff036d3653a505418850adecdc03d6e81df3d1
mihawk320 <carlosmora055@gmail.com>
2019-06-19 08:43:07 -0500

    Retos actualizados

---
+++ retosJS/Basic Javascript/51-reto.js
@@ -0,0 +1,16 @@
+// Example
+var sum = 0;
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+
+var sum=0;
+function addFive(){
+  sum= sum + 5;
+}
+
+
+// Only change code above this line
+var returnedValue = addFive();

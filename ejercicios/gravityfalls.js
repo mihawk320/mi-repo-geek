d593538cdf39081fe04d2f2d13a8893447f59b14
mihawk320 <carlosmora055@gmail.com>
2019-07-02 00:23:48 -0500

    actualizado

---
+++ ejercicios/gravityfalls.js
@@ -0,0 +1,33 @@
+var meta=0;
+var integrantes=["Wendy","Dipper","Soos","Mabel"];
+var dinero=[0,0,0,0];
+var total=0;
+var porcentajes=[0,0,0,0];
+meta=prompt("Ingrese la meta de dinero para la cabaña");
+var metaPersonal=meta/4;
+alert("La meta de cada uno es " + metaPersonal);
+
+    for(var i=0;i<integrantes.length;i++){
+        var money=prompt("Ingrese el dinero que recogio "+integrantes[i]);
+        dinero[i]+=money;
+    
+        total+=dinero[i];
+    
+        if(dinero[i]>metaPersonal){
+         document.write(integrantes[i]+ " Recogio el dinero, " + "(" + parseInt(dinero[i]) + " dolares)" + "<br>")
+        }
+        else{
+         document.write(integrantes[i]+ " No recogio lo suficiente, " + "(" + parseInt(dinero[i]) + " dolares)" + "<br>");
+        }
+    }
+
+if(total>meta){
+    document.write("<h2>Se logró cumplir con la meta asignada</h2> <br>")
+}
+else{
+    document.write("<h2>No se logró cumplir con la meta asignada</h2> <br>")
+}
+
+for(var o=0;o<porcentajes.length;o++){
+    document.write("El porcentaje de "+ integrantes[o]+" en el total es de "+ (dinero[o]/total*100)+"<br>");
+}

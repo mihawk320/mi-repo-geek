fad6c72e9dd580fcfd87a237a80cd002870d0a01
mihawk320 <carlosmora055@gmail.com>
2019-07-08 12:07:47 -0500

    actualizado

---
+++ retosJS/Basic Data Structures/reto1.js
@@ -0,0 +1,1 @@
+let yourArray=["Lol",8,true,4,null]; // change this line
---
+++ retosJS/Basic Javascript/96-reto.js
@@ -0,0 +1,17 @@
+// Example
+var ourArr = [ 9, 10, 11, 12];
+var ourTotal = 0;
+
+for (var i = 0; i < ourArr.length; i++) {
+  ourTotal += ourArr[i];
+}
+
+// Setup
+var myArr = [ 2, 3, 4, 5, 6];
+var total=0;
+
+
+// Only change code below this line
+for(var l=0;l<myArr.length;l++){
+  total+=myArr[l];
+}
